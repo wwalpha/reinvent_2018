4b93c917f3eee22fccbc4e96b5aed146385784d9
wwalpha <wwalpha@gmail.com>
2018-12-10 20:42:32 +0900

    Update app.ts

--- lambda_layer/src/lambda/app.ts
+++ lambda_layer/src/lambda/app.ts
@@ -2,7 +2,9 @@ import * as utils from 'library/utils';
 import * as moment from 'moment';
 
 export const handler = (event: any, context: any, callback: any) => {
+  // 共通処理のライブラリ
   console.log(utils.test());
+  // 外部のライブラリ
   console.log(moment.now());
 
   callback(null, null);
